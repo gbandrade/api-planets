681dddedf43335e53412e04636e3c647170dbfa3
gbandrade <gbandrade91@gmail.com>
2020-09-17 20:04:35 -0300

    Swapi api properties configuration

--- src/main/java/br/com/planets/config/ApplicationProperties.java
+++ src/main/java/br/com/planets/config/ApplicationProperties.java
@@ -10,4 +10,44 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+	
+	private final Api api = new Api();
+	
+	public static class Api {
+		private String path;
+		private Endpoint endpoint;
+
+		public String getPath() {
+			return path;
+		}
+
+		public void setPath(String path) {
+			this.path = path;
+		}
+
+		public Endpoint getEndpoint() {
+			return endpoint;
+		}
+
+		public void setEndpoint(Endpoint endpoint) {
+			this.endpoint = endpoint;
+		}
+	}
+	
+	public static class Endpoint {
+		private String getPlanets;
+
+		public String getGetPlanets() {
+			return getPlanets;
+		}
+
+		public void setGetPlanets(String getPlanets) {
+			this.getPlanets = getPlanets;
+		}
+	}
+
+	public Api getApi() {
+		return api;
+	}
+
 }
